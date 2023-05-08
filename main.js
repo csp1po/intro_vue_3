662223c3ab7cd5a6c98872a638ae53a560691ef9
Paulo Oliveira <paulo.cf.oliveira@gmail.com>
2023-05-07 21:30:20 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            cart: [],
+            premium: true
+        }
+    },
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+        }
+    }
+  })
+  
